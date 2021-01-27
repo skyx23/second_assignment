b0a9af5e491e12b992388aa4ec1d7435f168ad90
skyx23 <skydragonx23@gmail.com>
2021-01-27 11:02:32 +0530

    added enviroment files

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ const app = express();
 const mongoose = require('mongoose');
 // importing defined routes 
 const routes = require('./routes/route');
+// importing dotenv
+const dotenv = require('dotenv');
 
+// initializing dotenv file
+dotenv.config();
 
-//connection URI 
-const url = 'mongodb+srv://admin:7blQQYL6MrnIenru@project1.mz5o4.mongodb.net/user?retryWrites=true&w=majority';
 
 // connecting to Database
-mongoose.connect(url, {useNewUrlParser: true ,useUnifiedTopology : true},() => console.log(`connected`));
+mongoose.connect(process.env.CONNECTION_URI, {useNewUrlParser: true ,useUnifiedTopology : true},() => console.log(`connected`));
 
 // getting port from enviroment
 const port = process.env.port || 5000 ; 
